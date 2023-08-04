604a3d69f503cd70d1ce2a6a7e169e928caa3270
Erkan Demir <erkandemir86@gmail.com>
2023-08-05 00:12:33 +0100

    settings update

--- WebApi/settings.py
+++ WebApi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p!+95&e%u1n-hug8by_2rn-!@r%-#rycogz_2z%b6i!mcrx993'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "192.168.0.73", "10.3.224.213", "10.21.85.198", "104.131.12.252"]
 
