6c6b7d115b238f20b3059196db8779b926373eb4
Erkan Demir <erkandemir86@gmail.com>
2023-08-05 23:43:06 +0100

    unit tests added

--- eventfinder/tests.py
+++ eventfinder/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.test import Client
+from eventfinder.models import EventCategory
+from eventfinder.serializer import EventCategorySerializer
+from eventfinder.models import Event
+from eventfinder.serializer import Event
+ 
+#Test: if all eventCategory from database and from api equal
+class EventCategoryTestCase(TestCase):
+    def test_eventcategorylist(self):
+        url = reverse('eventcategory-list')
+        response = self.client.get(url)
+        eventCategories = EventCategory.objects.all()
+        expected_data = EventCategorySerializer(eventCategories, many=True).data
+        assert response.status_code == 200
+        self.assertEqual(response.data, expected_data)
 
-# Create your tests here.
+#Test: if all event from database and from api equal
+class EventTestCase(TestCase):
+    def test_eventlist(self):
+        url = reverse('event-list')
+        response = self.client.get(url)
+        events = Event.objects.all()
+        expected_data = EventCategorySerializer(events, many=True).data
+        assert response.status_code == 200
+        self.assertEqual(response.data, expected_data)
